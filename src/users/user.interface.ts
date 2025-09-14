a438986bb48c4ff250236d377d6a79c31b88f370
xav <xav1234g@gmail.com>
2025-09-14 23:41:17 +0800

    Initialize REST API with user management features, including environment configuration, routing, and database operations.

---
+++ src/users/user.interface.ts
@@ -0,0 +1,14 @@
+// src/users/user.interface.ts
+export interface User {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface UnitUser extends User {
+  id: string;
+}
+
+export interface Users {
+  [key: string]: UnitUser;
+}
